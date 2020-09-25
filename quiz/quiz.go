9a7b54e7aa6531af0bbe2b945afc6b78358f1c8d
Ryan Tang <ryantang@gmail.com>
2020-09-24 21:08:35 -0700

    Add flag to specify a different CSV quiz file

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,7 +12,10 @@ import (
 )
 
 func main() {
-	records := loadQuizCSV("problems.csv")
+	quizCSV := flag.String("file", "problems.csv", "Quiz CSV file in 'question,answer' format")
+	flag.Parse()
+
+	records := loadCSV(*quizCSV)
 	// assume records structure:
 	// [
 	//  ["question1, "answer1"],
@@ -25,27 +29,30 @@ func main() {
 
 	for _, s := range records {
 		fmt.Print(s[0], ": ")
+
 		answer, _ := consoleReader.ReadString('\n')
-		answer = strings.Replace(answer, "\n", "", -1)
+		answer = strings.ReplaceAll(answer, "\n", "")
 
 		if answer == s[1] {
-			correctAnswers = correctAnswers + 1
+			correctAnswers++
 		}
 	}
 
 	fmt.Print("Correct answers: ", correctAnswers, "/", totalQuestions, "\n")
 }
 
-func loadQuizCSV(csvFile string) [][]string {
-	data, err := ioutil.ReadFile("problems.csv")
+func loadCSV(csvFile string) [][]string {
+	data, err := ioutil.ReadFile(csvFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	r := csv.NewReader(strings.NewReader(string(data)))
 	records, err := r.ReadAll()
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	return records
 }
