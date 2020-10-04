15674372c3e901b6323f6c4886e7003ca68eb66c
Ryan Tang <ryantang@gmail.com>
2020-10-04 00:08:09 -0700

    Add inital solution to URL Shortener
    *Fill in MapHandler function
    *Fill in YAMLHandler function, which calls MapHandler

---
+++ urlshort/handler.go
@@ -0,0 +1,66 @@
+package urlshort
+
+import (
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		newSite := pathsToUrls[r.URL.String()]
+		if newSite != "" {
+			http.Redirect(w, r, newSite, 302)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+type pathURL struct {
+	Path string
+	Url  string
+}
+
+type pathURLs []pathURL
+
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var redirects pathURLs
+
+	err := yaml.Unmarshal(yml, &redirects)
+	if err != nil {
+		return nil, err
+	}
+
+	return MapHandler(redirects.toMap(), fallback), nil
+}
+
+func (p pathURLs) toMap() map[string]string {
+	m := make(map[string]string)
+	for _, entry := range p {
+		m[entry.Path] = entry.Url
+	}
+	return m
+}
